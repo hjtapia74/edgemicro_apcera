8717687ec252abd73846324a4f98c638b01d4582
Hector Tapia <hector@Hector-Tapia-MBP.local>
2016-04-13 09:16:53 -0700

    EdgeMicro 1.1.2 + Persistence of the Configuration
    This version includes the Apigee Edge Micro 1.1.2 and also uses NFS to
    persist the configuration of Edge Micro, the startup script checks for
    the configuration file and decides if configuration is still needed or
    not.

--- edgemicro_app/bash_start.sh
+++ edgemicro_app/bash_start.sh
@@ -2,57 +2,43 @@
 
 set -e
 echo "*** Starting at $(date) ***"
-edge_root=/opt/apcera/apigee-edge-micro-1.1.0
+edge_root=/opt/apcera/apigee-edge-micro-1.1.2
 
-export PATH=$edge_root/cli/bin:$PATH
-echo Path is:
-echo ${PATH} | tr ":" "\n"
 
-ln -s /nfs/data/edgemicro-config-amer-partner5-test.yaml /var/tmp/edgemicro-config-amer-partner5-test.yaml
-ls -l /var/tmp
+if [ -e /nfs/data/.edgemicro/config.yaml ]
+  then
+      echo "*** Starting Edge Micro Agent ***"
+      sudo ln -s /nfs/data/.edgemicro /.edgemicro
+      more /.edgemicro/config.yaml
 
-cd $edge_root/cli/bin
+      export EDGEMICRO_KEY=$(awk '/key/ {print $2}' /nfs/data/.edgemicro/edgemicro_configuration)
+      echo "*** Key is ${EDGEMICRO_KEY} ***"
+      export EDGEMICRO_SECRET=$(awk '/secret/ {print $2}' /nfs/data/.edgemicro/edgemicro_configuration)
+      echo "*** Secret is ${EDGEMICRO_SECRET} ***"
+      
+      cd $edge_root/agent
+      npm start
+  else
+      echo "*** Creating directory /nfs/data/.edgemicro and linking to /.edgemicro"
+      sudo mkdir /nfs/data/.edgemicro
+      chown runner:runner /nfs/data/.edgemicro
+      sudo ln -s /nfs/data/.edgemicro /.edgemicro
 
-echo "*** Configuring Edge Micro ***"
+      cd $edge_root/cli/bin
 
-./edgemicro configure -o amer-partner5 -e test -u hector@apcera.com -p Cops0240 > edgemicro_configuration
+      echo "*** Configuring Edge Micro ***"
 
-export EDGEMICRO_KEY=$(awk '/key/ {print $2}' edgemicro_configuration)
-echo "*** Key is ${EDGEMICRO_KEY} ***"
-export EDGEMICRO_SECRET=$(awk '/secret/ {print $2}' edgemicro_configuration)
-echo "*** Secret is ${EDGEMICRO_SECRET} ***"
+      ./edgemicro configure -o apcera -e test -u hector@apcera.com -p Cops0240 > /nfs/data/.edgemicro/edgemicro_configuration
 
-echo "*** Verifying Edge Micro ./edgemicro verify -o amer-partner5 -e test -k ${EDGEMICRO_KEY} -s ${EDGEMICRO_SECRET} ***"
-./edgemicro verify -o amer-partner5 -e test -k ${EDGEMICRO_KEY} -s ${EDGEMICRO_SECRET}
+      export EDGEMICRO_KEY=$(awk '/key/ {print $2}' /nfs/data/.edgemicro/edgemicro_configuration)
+      echo "*** Key is ${EDGEMICRO_KEY} ***"
+      export EDGEMICRO_SECRET=$(awk '/secret/ {print $2}' /nfs/data/.edgemicro/edgemicro_configuration)
+      echo "*** Secret is ${EDGEMICRO_SECRET} ***"
 
-#echo "*** PAUSE A BIT ***"
-#sleep 10
+      echo "*** Verifying Edge Micro ./edgemicro verify -o apcera -e test -k ${EDGEMICRO_KEY} -s ${EDGEMICRO_SECRET} ***"
+      ./edgemicro verify -o apcera -e test -k ${EDGEMICRO_KEY} -s ${EDGEMICRO_SECRET}
 
-echo "*** Starting Edge Micro Agent ***"
-cd $edge_root/agent
-
-#echo Shell is $SHELL
-#echo working in  $(pwd)
-#echo using this nohup $(which nohup)
-#echo dir permissions $(ls -ld .)
-
-#nohup_file=/tmp/nohup.out
-#nohup npm start &> $nohup_file &
-npm start
-
-#echo pause a bit
-#sleep 10
-#cat $nohup_file | awk '{printf("\t%s\n", $0)}'
-
-#echo check port 9000
-#netstat -na | egrep "8000|9000"
-
-#echo "Starting Edge Microgateway"
-#cd $edge_root/cli/bin
-
-#./edgemicro agent proc -c start -k ${KEY} -s ${SECRET}
-
-#echo "Finished at $(date)"
-#netstat -na | egrep "8000|9000"
-
-#tail -100f $nohup_file
+      echo "*** Starting Edge Micro Agent ***"
+      cd $edge_root/agent
+      npm start
+fi
---
+++ weather_api/weather.js
@@ -0,0 +1,57 @@
+var express  = require('express'),
+    YQL      = require('yql'),
+    re1      = new RegExp('[0-9]{5}'),
+    app      = express();
+
+// Generic Send Error Function
+function sendError(res, code, msg) {
+  res.status(code);
+  res.json({ error: msg });
+}
+
+// GET /forecast
+app.get('/forecast', function(request, response) {
+  try {
+    // is the zipcode parameter present and valid?
+    if (!request.query.zipcode || !request.query.zipcode.match(re1)) {
+      sendError(response, 400, 'Missing or invalid query parameter - zipcode');
+    }
+    else {
+      // create the query per YQL module documentation & then execute the query
+      var forecastQuery = 'SELECT * FROM weather.forecast WHERE (location = ' +
+        request.query.zipcode + ')';
+      var query = new YQL(forecastQuery);
+      // execute the query and then asynchronously create/send the
+      // response in the anonymous callback function.
+      query.exec(function(error, data) {
+        if (error) {
+          sendError(response, 500, error);
+        }
+        else {
+          response.json({
+            location : data.query.results.channel.location,
+            units : data.query.results.channel.units,
+            condition : data.query.results.channel.item.condition,
+            forecast : data.query.results.channel.item.forecast
+          });
+        }
+      });
+    }
+  }
+  catch(e) {
+      sendError(response, 500, "Internal Server Error - " + e.message);
+    }
+  });
+
+  // catch and return 404
+  app.use(function(req, res, next) {
+    res.status(404);
+    res.json({ message: "Not found" });
+  });
+
+  // start the server
+  var listener = app.listen(process.env.PORT || 7000, function() {
+        var host = listener.address().address;
+        var port = listener.address().port;
+        console.log('server listening at http://%s:%s', host, process.env.PORT);
+      });
